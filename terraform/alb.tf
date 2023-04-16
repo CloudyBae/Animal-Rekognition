dce17af14f5a41fb7b7b35921963d3a32564b806
Yoojin93 <yoojinbae93@gmail.com>
2023-04-15 19:36:44 -0500

    updates

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -31,8 +31,11 @@ resource "aws_security_group" "animalrekog_alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/"]
+ }
+} 
